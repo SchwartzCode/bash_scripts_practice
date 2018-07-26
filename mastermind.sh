368f8d37fe401e4eda709e8f0504f49c9bde486a
Jonathan Schwartz <jonbs98@gmail.com>
2018-07-26 17:48:18 -0400

    Finished script for mastermind game

--- mastermind.sh
+++ mastermind.sh
@@ -2,6 +2,7 @@
 answer=""
 COLORS=("R" "B" "G" "Y" "W" "P")
 ANSWER_LENGTH=4
+GUESS_COUNT=0
 
 #generates solution that user is trying to guess
 
@@ -11,16 +12,13 @@ for ((i=0;i<ANSWER_LENGTH; i++)); do
   answer+=${COLORS[$rand_num]}
 done
 
-echo $answer
 
 check_guess () {
-  
+    NUM_CORRECT=0
 
   for ((i=0;i<ANSWER_LENGTH; i++)); do
-    #printf "$i:\t"
     guess_val=${guess:${i-1}:1}
     answer_val=${answer:${i-1}:1}
-
     #printf "GUESS_VAL: $guess_val \t ANSWER_VAL: $answer_val \n"
     
     if [ "$guess_val" == "$answer_val" ]
@@ -29,43 +27,64 @@ check_guess () {
         ((NUM_CORRECT++))
         #echo "NOT A MATCH"
     fi
-    
+
 
   done
 
 
+    ((GUESS_COUNT++))
 }
 
-  echo   "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
-  echo   "|||||                     $ANSWER_LENGTH MARBLES                           |||||"
-  echo   "|||||                                                         |||||"
-  echo   "|||||     COLORS: red [R], blue [B], green [G]                |||||"
-  echo   "|||||             yellow [Y], white [W], purple [P]           |||||"
-  echo   "|||||                                                         |||||"
-  echo   "|||||                    ANSWER:  $answer                        |||||"
-  echo   "|||||=========================================================|||||"
-  
+
+
+  output="\n\n\n|||||=========================================================|||||\n"
+  output+="|||||=========================================================|||||\n"
+  output+="|||||                                                         |||||\n"
+  output+="|||||                     $ANSWER_LENGTH MARBLES                           |||||\n"
+  output+="|||||                                                         |||||\n"
+  output+="|||||                                                         |||||\n"
+  output+="|||||     COLORS: red [R], blue [B], green [G]                |||||\n"
+  output+="|||||             yellow [Y], white [W], purple [P]           |||||\n"
+  output+="|||||                                                         |||||\n"
+  output+="|||||                                                         |||||\n"
+  #output+="|||||                    ANSWER:  $answer                        |||||\n"
+  output+="|||||=========================================================|||||\n"
+  output+="|||||            GUESS            ||      NUMBER_CORRECT      |||||\n"
+  output+="|||||=========================================================|||||\n"
+
   guess_correct=0
-while [ $guess_correct -lt 1 ]; do
 
-  printf "|||||        "
+  while [ $guess_correct -lt 1 ]; do
+  printf "$output"
+  
+
+
+  printf "\n\n\t\t Enter a guess:  "
   read -r guess 
-  echo -en "\e[1A"
-  echo   "              ||        NUM CORRECT         |||||"
-  echo   "|||||=========================================================|||||"
-
-  NUM_CORRECT=0
   check_guess
 
-  if [ "$guess" = "$answer" ]; then
+  output+="|||||            $guess             ||            $NUM_CORRECT             |||||\n"
+
+  if [ "$guess" = "$answer" ]; then 
   
     #echo -e '\r \r'
+    printf "$output"
     echo   "|||||=========================================================|||||"
     echo   "|||||=========================================================|||||"
-    echo   "|||||                       CONGRATULATIONS!                  |||||"
-    echo   "|||||                     YOU GUESSED THE CODE!               |||||"
     echo   "|||||                                                         |||||"
-    echo   "|||||                      ANSWER:   $answer                    |||||"
+    echo   "|||||                       CONGRATULATIONS!                  |||||"
+
+    if [ $GUESS_COUNT -lt 10 ]; then
+    echo   "|||||                 YOU GUESSED THE CODE IN $GUESS_COUNT GUESSES!      |||||"
+    elif [ $GUESS_COUNT -lt 100 ]; then
+    echo   "|||||                YOU GUESSED THE CODE IN $GUESS_COUNT GUESSES!      |||||"
+    else
+      echo "|||||               YOU GUESSED THE CODE IN $GUESS_COUNT GUESSES!      |||||"
+    fi
+
+    echo   "|||||                                                         |||||"
+    echo   "|||||                      ANSWER:   $answer                     |||||"
+    echo   "|||||                                                         |||||"
     echo   "|||||=========================================================|||||"
     echo   "|||||=========================================================|||||"
 
